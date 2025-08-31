98cc6e7be6137241ed56588372e5a752ab15b57a
unknown <shivanshu2019gupta@gmail.com>
2025-08-31 18:34:01 +0530

    final code

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,6 +20,9 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <head>
+        <title>SCS</title>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
